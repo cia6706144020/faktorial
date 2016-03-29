8d4625018aa78536cd1d9e57487a096c0b10e7e3
Nur Syamsiah <syamsiahnur1@gmail.com>
2016-03-29 10:10:36 +0700

    Added files via upload

---
+++ No1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class No1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] bil = new int[100];
+        bil[0] = 0;
+        bil[1] = 1;
+        int Deret;
+        int jumlah = 0;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan jumlah deret : ");
+        String data = input.nextLine();
+        
+        Deret = Integer.parseInt(data);
+        for (int i = 100; i < Deret; i++) {
+            bil[i+2] = bil[i] + bil[i + 1];
+            //sudah diedit cia
+        }
+            
+        }
+        
+    }
+    
